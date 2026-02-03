54d00d7d004312fbf234bbc79062635c09dd0287
Adhiksha007 <uppalapatiadhikshareddy@gmail.com>
2026-02-03 15:57:02 +0530

    Updated Files

--- frontend/src/pages/News.tsx
+++ frontend/src/pages/News.tsx
@@ -45,7 +45,7 @@ export default function News() {
                         layout
                         target="_blank"
                         rel="noopener noreferrer"
-                        initial={{ opacity: 0, y: 20 }}
+                        initial={{ opacity: 0, y: 50 }}
                         whileInView={{ opacity: 1, y: 0 }}
                         viewport={{ once: true }}
                         transition={{ duration: 0.5 }}
